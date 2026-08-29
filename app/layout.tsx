7639420f98c29f9fdcd36cfaf3d11718eed8d426
ElktrumElk <festinacole373@gmail.com>
2026-08-29 21:15:25 +0000

    ---

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,6 +70,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
   colorScheme: "light dark",
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "#fafafa" },
